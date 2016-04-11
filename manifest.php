2bf7cda24d0600615c95b7f5b8b5ca4d5e7a018c
Joel Bout <joel@taotesting.com>
2016-04-11 18:42:18 +0200

    Added empty extension shell

---
+++ manifest.php
@@ -0,0 +1,50 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */
+return array(
+    'name' => 'pcgCore',
+    'label' => 'PCG core extension',
+    'description' => '',
+    'license' => 'GPL-2.0',
+    'version' => '0.1',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'tao' => '>=2.22.1'
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#pcgCoreManager',
+    'acl' => array(
+        array('grant','http://www.tao.lu/Ontologies/generis.rdf#pcgCoreManager',array('ext' => 'pcgCore'))
+    ),
+    'install' => array(),
+    'uninstall' => array(),
+    'routes' => array(
+        '/pcgCore' => 'pcgroupUs\\pcgCore\\controller'
+    ),
+    'constants' => array(
+        // views directory
+        "DIR_VIEWS" => dirname(__FILE__) . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR,
+        
+        // BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL . 'pcgCore/',
+        
+        // BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL . 'pcgCore/views/'
+    )
+);
